6f3cdf7bb1e199ab8322998f966cd1e35a6a7433
Vasca Meevis <43817661+VascaM@users.noreply.github.com>
2023-09-18 10:36:52 +0200

    Update PythonATBS_Chapter4.py

--- Chapter4/PythonATBS_Chapter4.py
+++ Chapter4/PythonATBS_Chapter4.py
@@ -63,15 +63,97 @@ for index, item in enumerate(supplies):
 # Using the random.choice() and random.shuffle() Functions with Lists
 import random
 pets = ['Dog', 'Cat', 'Moose']
-print (random.choice(pets)
-)
+print (random.choice(pets))
 
 people = ['Alice', 'Bob', 'Carol', 'David']
-print (random.shuffle(people))
+random.shuffle(people)
+print(people)
 
 # Augmented Assignment Operators
 spam = 42
 spam += 1
 print (spam)
 
+# Finding a Value in a List with the index() Method
+spam = ['hello', 'hi', 'howdy', 'heyas']
+print (spam.index('hello'))
+
+# Adding Values to Lists with the append() and insert() Methods
+spam = ['cat', 'dog', 'bat']
+spam.append('moose')
+print (spam)
+spam = ['cat', 'dog', 'bat']
+spam.insert(1, 'chicken')
+print (spam)
+
+# Removing Values from Lists with the remove() Method
+spam = ['cat', 'bat', 'rat', 'elephant']
+spam.remove('bat')
+print (spam)
+
+# Sorting the Values in a List with the sort() Method
+spam = ['a', 'z', 'A', 'Z']
+spam.sort(key=str.lower)
+print (spam)
+
+# Reversing the Values in a List with the reverse() Method
+spam = ['cat', 'dog', 'moose']
+spam.reverse()
+print (spam)
+
+# Mutate mutable Data Types with append() Method
+eggs = [1, 2]
+del eggs[1]
+del eggs[0]
+eggs.append(3)
+eggs.append(4)
+print (eggs)
+
+# Mutate immutable Data Types by copying from parts
+name = 'Zophie a cat'
+newName = name[0:7] + 'the' + name[8:12]
+name = 'Zophie a cat'
+print (newName)
+
+# The Tuple Data Type
+eggs = ('hello', 42, 0.5) #() instead of [], tuples are immutable
+
+# Converting Types with the list() and tuple() Functions
+print(tuple(['cat', 'dog', 5]))
+print(list(('cat', 'dog', 5)))
+print(list('hello'))
+
+# References
+spam = 42
+cheese = spam
+spam = 100
+print (spam)
+print (cheese)
+
+spam = [0, 1, 2, 3, 4, 5]
+cheese = spam 		    # The reference is being copied, not the list
+cheese[1] = 'Hello!' 	    # This changes the list value
+print (spam)
+print (cheese)  	    # The cheese variable refers to the same list
+
+# id() Function
+print (id('Howdy'))
+
+# Passing References
+def eggs(cheese):
+    cheese.append('Hello')
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
+                #  Cheese gets destroyed, but it made changes to the same list spam refers to
+
+# The copy Module’s copy() Function
+import copy 
+spam = ['A', 'B', 'C', 'D']
+id(spam)								
+cheese = copy.copy(spam)
+id(cheese)                  # cheese is a different list with different identity.	
+cheese[1] = 42
+print (spam)
+print (cheese)						    
 
